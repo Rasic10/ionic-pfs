ea4491dc16d525758d37c4929fecc093f7235a0f
dusanradivojevic <r.dusan97@gmail.com>
2021-05-24 23:55:22 +0200

    aws upload front implemented

--- src/app/tab2/aws3-storage/aws3-storage.page.ts
+++ src/app/tab2/aws3-storage/aws3-storage.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AwsStorageService } from 'src/app/services/aws-storage.services';
+
+interface HTMLInputEvent extends Event {
+  target: HTMLInputElement & EventTarget;
+}
 
 @Component({
   selector: 'app-aws3-storage',
@@ -7,5 +12,20 @@ import { Component } from '@angular/core';
 })
 export class AWS3StoragePage {
 
-  constructor() {}
+  setEventForAddFiles: any;
+  constructor(private awsService: AwsStorageService) {}
+
+  ngOnInit(): void {
+  }
+
+  filesSelected(event: Event) {
+    this.setEventForAddFiles = event;
+  }
+
+  imageSelected() {
+    var file = (this.setEventForAddFiles as HTMLInputEvent).target.files[0];
+
+    this.awsService.uploadImage(file);
+  }
+
 }
